77f1142e3ee066f2891eb89946f07aae80a9dbd7
Jensen Lai <ninnu.0123p@gmail.com>
2023-01-04 17:35:36 +1000

    added login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let data;
+	export let form;
+</script>
+
+<form method="post" action="?/login" class="flex flex-col items-center w-full space-y-2 m-10">
+	<h1 class="text-3xl font-bold">Log In</h1>
+	<input type="email" name="email" class="input input-bordered bg-base-200">
+	<input type="password" name="password" class="input input-bordered bg-base-200">
+	<button type="submit" class="btn btn-primary bg-green-400">Submit</button>
+</form>
