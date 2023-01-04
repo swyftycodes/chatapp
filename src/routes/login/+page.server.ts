3bb1f595781c40dff1c95a5bfc39b4c6f90172e5
Jensen Lai <ninnu.0123p@gmail.com>
2023-01-04 19:39:47 +1000

    added login route

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from "@prisma/client";
+import { fail } from "@sveltejs/kit";
+
+const prisma = new PrismaClient();
+
+export const actions = {
+	login: async ({ cookies, request }) => {
+		const body = Object.fromEntries(await request.formData());
+
+		const user = await prisma.users.findUnique({
+			where: {
+				email: body.email
+			}
+		})
+		
+		// throw error when credentials are invalid
+		if (!user) {
+			// if user is not found throw error
+			await prisma.$disconnect()
+			return fail(400, { message: 'invalid credentials' });
+		} else {
+			// if password is incorrect throw error
+			const validpw = user.password == body.password;
+
+			if (!validpw) {
+				await prisma.$disconnect()
+				return fail(400, { message: 'invalid credentials' });
+			}
+		}
+
+		// TODO: update user with auth token
+
+		const authedUser = await prisma.users.update({
+			where: {
+				email: user.email
+			},
+			data: {
+				auth_token: crypto.randomUUID()
+			}
+		})
+		
+		cookies.set('session', authedUser.auth_token, {
+			path: '/',
+			httpOnly: true,
+			secure: true,
+			sameSite: 'strict'
+		});
+
+		await prisma.$disconnect()
+		return {
+			message: 'Success'
+		}
+	}
+}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
+	import toast from 'svelte-french-toast';
+	import { enhance } from '$app/forms';
+	
 	export let data;
 	export let form;
+
+	const submit = ({ data, form, cancel }) => {
+		const body = Object.fromEntries(data);
+
+		if (!body.email || !body.password) {
+			toast.error('all fields must be filled');
+			cancel();
+		}
+
+		return async ({ result }) => {
+			switch (result.type) {
+				case 'success':
+					toast.success(result.data.message)
+					break;
+				case 'failure':
+					toast.error(result.data.message)
+					break;
+			}
+		}
+	} 
 </script>
 
-<form method="post" action="?/login" class="flex flex-col items-center w-full space-y-2 m-10">
-	<h1 class="text-3xl font-bold">Log In</h1>
+<form method="post" action="?/login" class="flex flex-col items-center w-full space-y-2" use:enhance={submit}>
+	<h1 class="text-3xl font-bold m-10">Log In</h1>
 	<input type="email" name="email" class="input input-bordered bg-base-200">
 	<input type="password" name="password" class="input input-bordered bg-base-200">
 	<button type="submit" class="btn btn-primary bg-green-400">Submit</button>
