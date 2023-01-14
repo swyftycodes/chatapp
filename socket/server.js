88fbeb69d35510a2e89e2149f31722e33c283c85
Jensen Lai <ninnu.0123p@gmail.com>
2023-01-14 16:07:13 +1000

    removed logging of socket id on connection

--- socket/server.js
+++ socket/server.js
@@ -2,15 +2,14 @@ import { Server } from 'socket.io';
 
 const io = new Server(3000, {
 	cors: {
-		origin: ['http://localhost:5173']
+		origin: ['http://192.168.1.15:5173']
 	}
 });
 
 io.on('connection', socket => {
-	console.log(socket.id);
-
-	socket.on('message', (content) => {
-		console.log(`${content}`);
-		io.emit('recieve-message', content);
+	socket.on('message', (data) => {
+		console.log(data)
+		
+		io.emit('recieve-message', data)
 	})
 })
